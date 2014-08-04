8da8193f95aa2eb6f2f4ebb19a5d9d41acd99bda
Balaji Athreya <athreya86@gmail.com>
2014-08-03 19:45:58 -0700

    initial commit

--- js/project.js
+++ js/project.js
@@ -1,49 +1,17 @@
 var feeds = [];
-  
-angular.module('feedModule', ['ngResource','ngSanitize'])
-  .factory('FeedLoader', function ($resource) {
-    return $resource('http://ajax.googleapis.com/ajax/services/feed/load', {}, {
-      fetch: { method: 'JSONP', params: {v: '1.0', callback: 'JSON_CALLBACK'} }
-    });
-  })
-  .factory('FeedList', function ($rootScope, FeedLoader) {
-    return function(zipcode) {
-      var feedSources = [
-        {title: 'Fandango', url: 'http://www.fandango.com/rss/moviesnearme_'+zipcode+'.rss'}
-      ];
-      if (feeds.length === 0) {
-        for (var i=0; i<feedSources.length; i++) {
-          FeedLoader.fetch({q: feedSources[i].url, num: 10}, {}, function (data) {
-            var feed = data.responseData.feed;
-            feeds.push(feed);
-          });
-        }
-      }
-      return feeds;
-    };
-  })
-  .factory('RTRatings', function ($rootScope, FeedLoader) {
-    return function(zipcode) {
-      var feedSources = [
-        {title: 'Fandango', url: 'http://www.fandango.com/rss/moviesnearme_'+zipcode+'.rss'}
-      ];
-      if (feeds.length === 0) {
-        for (var i=0; i<feedSources.length; i++) {
-          FeedLoader.fetch({q: feedSources[i].url, num: 10}, {}, function (data) {
-            var feed = data.responseData.feed;
-            feeds.push(feed);
-          });
-        }
-      }
-      return feeds;
-    };
-  })
-  .controller('FeedController', function ($scope, FeedList, RTRatings) {
-    this.zipCode = '';
-    this.zipCodeSubmit = function(){
-      $scope.feeds = new FeedList(this.zipCode);
-      $scope.$on('FeedList', function (event, data) {
-        $scope.feeds = data;
-      }); 
-    };    
+
+angular.module('feedModule', [])
+.controller('FeedController', function ($scope, $http) {
+  $scope.feed = {};
+  $scope.status = '';
+  var responsePromise = $http.jsonp("http://trimethelper.balajiathreya.com/getdashboard");
+
+  responsePromise.success(function(data, status, headers, config) {
+    $scope.status = 'Status : Success';
+    $scope.feed = data;
   });
+
+  responsePromise.error(function(data, status, headers, config) {
+    $scope.status = "Status : No data. Server returned response status: " + status;
+  });
+});

ae9599f6584658e2aeb6849391e2d603c5b4a721
balajiathreya <athreya86@gmail.com>
2014-08-10 14:38:08 -0400

    dashboard

--- js/project.js
+++ js/project.js
@@ -1,9 +1,10 @@
-var feeds = [];
+
 
 angular.module('feedModule', [])
 .controller('FeedController', function ($scope, $http) {
   $scope.feed = {};
   $scope.status = '';
+  $scope.showDetour = false;
   var responsePromise = $http.get("http://trimethelper.balajiathreya.com/getdashboard");
 
   responsePromise.success(function(data, status, headers, config) {
@@ -20,6 +21,14 @@ angular.module('feedModule', [])
     diff = new Date() - arrivaldate;
     millisdiff = Math.abs(diff);
     return Math.round( millisdiff / 60000 );
-  }
+  };
 
+  $scope.getDetourInfo = function(detour, detours){
+    id = detour[0];
+    return detours[id];
+  };
+  
+  $scope.toggleShowDetour = function(){
+    $scope.showDetour = !$scope.showDetour;
+  };
 });

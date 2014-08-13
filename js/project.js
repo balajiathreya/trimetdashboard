5b6b6c82b62c564d57d2f946cccaae6e36d6abbf
baathreya <baathreya@ebay.com>
2014-08-13 13:11:37 -0700

    show detour behavior chagne

--- js/project.js
+++ js/project.js
@@ -4,12 +4,14 @@ angular.module('feedModule', [])
 .controller('FeedController', function ($scope, $http) {
   $scope.feed = {};
   $scope.status = '';
-  $scope.showDetour = false;
   var responsePromise = $http.get("http://trimethelper.balajiathreya.com/getdashboard");
 
   responsePromise.success(function(data, status, headers, config) {
     $scope.status = 'Status : Success';
     $scope.feed = data;
+    for(i in $scope.feed.arrivals){
+      $scope.feed.arrivals[i].showDetour = true;
+    }
   });
 
   responsePromise.error(function(data, status, headers, config) {
@@ -22,13 +24,10 @@ angular.module('feedModule', [])
     millisdiff = Math.abs(diff);
     return Math.round( millisdiff / 60000 );
   };
-
-  $scope.getDetourInfo = function(detour, detours){
+  
+   $scope.getDetourInfo = function(detour, detours){
     id = detour[0];
     return detours[id];
   };
-  
-  $scope.toggleShowDetour = function(){
-    $scope.showDetour = !$scope.showDetour;
-  };
+
 });

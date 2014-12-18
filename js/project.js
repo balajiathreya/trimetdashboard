c9f83e8d51f47525a3a825e1fba1d3a7d74f083b
balajiathreya <athreya86@gmail.com>
2014-12-18 16:58:37 -0500

    nearby changes

--- js/project.js
+++ js/project.js
@@ -1,67 +1,115 @@
 
-
 angular.module('feedModule', [])
 .controller('FeedController', function ($scope, $http) {
   $scope.favStops = {};
   $scope.nearbyStops = {};
-  $scope.favStopsStatus = '';
-  $scope.nearbyStopsStatus = '';
-/*
-  coords = geolocation.getLocation().then(function(data){
-      return {lat:data.coords.latitude, long:data.coords.longitude};
-  });
 
-  // nearby locations
-  var nearbyStopsResponsePromise = $http.get("http://trimethelper.balajiathreya.com/getnearbystops", {
+  $scope.favLocIDs = "1003,1114,9978,10168,9833,9834,8381,9818";
+  $scope.nearbyLocIDS = '';
+
+  getGeoLocation($scope.lon, $scope.lat);
+  getFavLocIDsRoutes($scope.favLocIDs);
+
+// getRoutes(getNearbyLocIDs(),$scope.nearbyStopsStatus, $scope.nearbyStops);
+   
+  function getGeoLocation() {
+     if(navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+                lat = position.coords.latitude;
+                lon = position.coords.longitude;
+		getNearbyStops(lon, lat);
+        });
+     }    
+  }
+
+
+  function getNearbyStops(lon, lat) {
+      var responsePromise = $http.get("http://trimethelper.balajiathreya.com/getnearbystops", {
                                         params : {
-                                                "ll" : coords.long+","+coords.lat
+                                                "ll" : lon+","+lat
+                                         }
+                                 });
+/*
+      nearbyStopsResponsePromise.success(function(data, status, headers, config) {
+          $scope.nearbyStops = data;
+          $scope.nearbyLocIDs = getNearbyLocIDs().join();
+	  getNearbyLocIDsRoutes($scope.nearbyLocIDs);
+      });
+
+      nearbyStopsResponsePromise.error(function(data, status, headers, config) {
+          $scope.nearbyStops = data;
+      });
+*/
+	responsePromise.success(function(data, status, headers, config) {
+           $scope.nearbyStops = data;
+           for(i in $scope.nearbyStops.arrivals) {
+              $scope.nearbyStops.arrivals[i].showDetour = true;
+           }
+       });
+
+       responsePromise.error(function(data, status, headers, config) {
+           console.log('error: ' + data + " status: " + status);
+           $scope.nearbyStops = data;
+       });
+
+   };
+
+   function getNearbyLocIDs() {
+       nearbylocids = [];
+       for (i in $scope.nearbyStops.locations) {
+           loc = $scope.nearbyStops.locations[i];
+	   nearbylocids.push(loc.locid);
+       }
+       return nearbylocids;
+   }
+
+   function getFavLocIDsRoutes(locids){
+       var responsePromise = $http.get("http://trimethelper.balajiathreya.com/getfavoritestops", {
+                                        params : {
+                                                "locids" : locids
                                          }
                                  });
 
-  nearbyStopsResponsePromise.success(function(data, status, headers, config) {
-    $scope.nearbyStopsStatus = 'Status : Success';
-    $scope.nearbyStops = data;
-    for(i in $scope.nearbyStops.arrivals){
-      $scope.nearbyStops.arrivals[i].showDetour = true;
-    }
-  });
+       responsePromise.success(function(data, status, headers, config) {
+           $scope.favStops = data;
+           for(i in $scope.favStops.arrivals) {
+              $scope.favStops.arrivals[i].showDetour = true;
+           }
+       });
 
-  nearbyStopsResponsePromise.error(function(data, status, headers, config) {
-    $scope.nearbyStopsStatus = "Status : No data. Server returned response status: " + status;
-    $scope.nearbyStops = data;
-  });
+       responsePromise.error(function(data, status, headers, config) {
+	   console.log('error: ' + data + " status: " + status);
+           $scope.favStops = data;
+       });
+   }
 
-*/
 
-  // favorite locations 
-  var favLocsResponsePromise = $http.get("http://trimethelper.balajiathreya.com/getfavoritelocs", { 
-					params : { 
-						"locids" : "1003,1114,9978,10168,9833,9834,8381,9818"
-					 }
-				 });
+  function getNearbyLocIDsRoutes(locids) {
+      var responsePromise = $http.get("http://trimethelper.balajiathreya.com/getnearbystops", {
+                                        params : {
+                                                "locids" : locids
+                                         }
+                                 });
 
-  favLocsResponsePromise.success(function(data, status, headers, config) {
-    $scope.favStopsStatus = 'Status : Success';
-    $scope.favStops = data;
-    for(i in $scope.favStops.arrivals){
-      $scope.favStops.arrivals[i].showDetour = true;
-    }
-  });
+       responsePromise.success(function(data, status, headers, config) {
+           $scope.nearbyStops = data;
+           for(i in $scope.nearbyStops.arrivals) {
+              $scope.nearbyStops.arrivals[i].showDetour = true;
+           }
+       });
 
-  favLocsResponsePromise.error(function(data, status, headers, config) {
-    $scope.favStopsStatus = "Status : No data. Server returned response status: " + status;
-    $scope.favStops = data;
-  });
+       responsePromise.error(function(data, status, headers, config) {
+           console.log('error: ' + data + " status: " + status);
+           $scope.nearbyStops = data;
+       });
+  }
+
+
+
 
   $scope.getMinutes = function(arrivaldate){
     diff = new Date() - arrivaldate;
     millisdiff = Math.abs(diff);
     return Math.round( millisdiff / 60000 );
   };
-
-
- 
-
-
-  
 });
